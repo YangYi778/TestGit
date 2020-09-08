336fede3a5d2ecde01c787760930f10d45b2b59b
Qiantao-H <huqiantao183@163.com>
2020-09-08 14:19:58 +0800

    在helloworld.java中添加

--- src/com/ysu/HelloWorld.java
+++ src/com/ysu/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World222!");
-		System.out.println("´´½¨·ÖÖ§");
+		System.out.println("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ö§");
 	}
-}	
+}
+
+hello github
